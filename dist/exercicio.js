b57dd5abb3fef50c9fb2b6e4506f5304518638da
MurilloPsc <murillopereira@outlook.com>
2019-08-07 22:00:47 -0300

    Exercicio1

---
+++ dist/exercicio.js
@@ -0,0 +1,13 @@
+var nota1 = 10;
+var nota2 = 9;
+var nota3 = 5;
+var media = 0;
+media = nota1 + nota2 + nota3;
+media = media / 3;
+if (function (media) { return 7; }) {
+    console.log("Aprovado");
+}
+else {
+    console.log("Reprovado");
+}
+console.log("A média é " + media);
---
+++ exercicio.ts
@@ -0,0 +1,21 @@
+let nota1 : number = 10
+let nota2 : number = 9
+let nota3 : number = 5
+let media : number = 0
+
+media = nota1 + nota2 + nota3 
+media = media/3
+
+if (media => 7){
+ console.log("Aprovado")
+}
+else{
+console.log("Reprovado")
+
+}
+ 
+console.log("A média é " + media)
+
+
+
+
